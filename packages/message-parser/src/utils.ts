ae00e8bf83a940e48edcebe653d1b8b502f5f586
amitkumarashutosh <amitkumarashutos@gmail.com>
2026-04-02 12:37:16 +0530

    fix(parser): IP URL linking, emphasis URL detection, empty blockquotes, lineBreak export

--- packages/message-parser/src/utils.ts
+++ packages/message-parser/src/utils.ts
@@ -82,7 +82,7 @@ export const link = (src: string, label?: Markup[]): Link => ({
 export const autoLink = (src: string, customDomains?: string[]): Plain | Link => {
 	const validHosts = ['localhost', ...(customDomains ?? [])];
 	const { isIcann, isIp, isPrivate, domain } = tldParse(src, {
-		detectIp: false,
+		detectIp: true,
 		allowPrivateDomains: true,
 		validHosts,
 	});
